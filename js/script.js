f631bc88598d8b39206a343e9dac7f4db6b9367a
RAS <raulaguirreshiguemoto@gmail.com>
2021-05-02 21:00:54 -0300

    Adicionando unid km e validacao de campos

--- js/script.js
+++ js/script.js
@@ -3,46 +3,54 @@ function converter() {
 	let select2 = document.getElementById('select2').value
 
 	let valor = document.getElementById('valor').value
+	let resultado = document.getElementById('resultado')
 
-	// CONVERTER METROS EM CENTÍMETROS
-	if (select1 === '1' && select2 === '2') {
-		let resultado = document.getElementById('resultado')
+	console.log(select1)
 
-		resultado.innerHTML = valor * 100 + ' CENTÍMETROS'
-	}
+	if (select1 === 'SELECIONE...' || select2 === 'SELECIONE...') {
+		alert('UNIDADE DE MEDIDA PRECISA SER INFORMADA!')
 
-	// CONVERTER METROS EM MILÍMETROS
-	if (select1 === '1' && select2 === '3') {
-		let resultado = document.getElementById('resultado')
+	} else if (valor === '') {
+		alert('UM VALOR PRECISA SER INFORMADO!')
 
-		resultado.innerHTML = valor * 1000 + ' MILÍMETROS'
-	}
+	} else {
 
-	// CONVERTER CENTÍMETROS EM METROS
-	if (select1 === '2' && select2 === '1') {
-		let resultado = document.getElementById('resultado')
+		if (select1 === '0' && select2 === '1') { // CONVERTER KM EM METROS
+			resultado.innerHTML = valor * 1000 + ' METROS'
 
-		resultado.innerHTML = valor / 100 + ' METROS'
-	}
+		} else if (select1 === '0' && select2 === '2') { // CONVERTER KM EM CENTÍMETROS
+			resultado.innerHTML = valor * 1000 * 100 + ' CENTÍMETROS'
 
-	// CONVERTER CENTÍMETROS EM MILÍMETROS
-	if (select1 === '2' && select2 === '3') {
-		let resultado = document.getElementById('resultado')
+		} else if (select1 === '0' && select2 === '3') { // CONVERTER KM EM MILÍMETROS
+			resultado.innerHTML = valor * 1000 * 100 * 10 + ' MILÍMETROS'
 
-		resultado.innerHTML = valor * 10 + ' MILÍMETROS'
-	}
+		} else if (select1 === '1' && select2 === '0') { // CONVERTER METROS EM KM
+			resultado.innerHTML = valor / 1000 + ' KM'
 
-	// CONVERTER MILÍMETROS EM METROS
-	if (select1 === '3' && select2 === '1') {
-		let resultado = document.getElementById('resultado')
+		} else if (select1 === '1' && select2 === '2') { // CONVERTER METROS EM CENTÍMETROS
+			resultado.innerHTML = valor * 100 + ' CENTÍMETROS'
 
-		resultado.innerHTML = valor / 1000 + ' METROS'
-	}
+		} else if (select1 === '1' && select2 === '3') { // CONVERTER METROS EM MILÍMETROS
+			resultado.innerHTML = valor * 1000 + ' MILÍMETROS'
 
-	// CONVERTER MILÍMETROS EM CENTÍMETROS
-	if (select1 === '3' && select2 === '2') {
-		let resultado = document.getElementById('resultado')
+		} else if (select1 === '2' && select2 === '0') { // CONVERTER CENTÍMETROS EM KM
+			resultado.innerHTML = valor / 100 / 1000 + ' KM'
 
-		resultado.innerHTML = valor / 10 + ' CENTÍMETROS'
+		} else if (select1 === '2' && select2 === '1') { // CONVERTER CENTÍMETROS EM METROS
+			resultado.innerHTML = valor / 100 + ' METROS'
+
+		} else if (select1 === '2' && select2 === '3') { // CONVERTER CENTÍMETROS EM MILÍMETROS
+			resultado.innerHTML = valor * 10 + ' MILÍMETROS'
+
+		} else if (select1 === '3' && select2 === '0') { // CONVERTER MILÍMETROS EM KM
+			resultado.innerHTML = valor / 1000 / 1000 + ' KM'
+
+		} else if (select1 === '3' && select2 === '1') { // CONVERTER MILÍMETROS EM METROS
+			resultado.innerHTML = valor / 1000 + ' METROS'
+
+		} else if (select1 === '3' && select2 === '2') { // CONVERTER MILÍMETROS EM CENTÍMETROS
+			resultado.innerHTML = valor / 10 + ' CENTÍMETROS'
+
+		}
 	}
 }
